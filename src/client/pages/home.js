4c733e172e932643617a590566581d918333c1bc
imornar <ivan.mornar@toptal.com>
2018-11-03 19:46:20 +0100

    shit

--- src/client/pages/home.js
+++ src/client/pages/home.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <div className="center-align" style={{ marginTop: 300 }}>
-        <h3>Welcome</h3>
-        <p>ssr</p>
+      <div>
+        <div className="center-align" style={{ marginTop: 300 }}>
+          <h3>format1</h3>
+          <p>format2</p>
+        </div>
       </div>
-    </div>
   )
 };
 
